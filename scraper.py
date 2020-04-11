c697ff15b5af0bb589a72e84f3dc84996896f489
Vedant Tilwani <vptilwani@gmail.com>
2020-04-11 19:26:06 +0530

    first commit

--- scraper.py
+++ scraper.py
@@ -1,3 +1,11 @@
+'''
+A python web scraper that scrapers egg.com looking for all the graphics cards and 
+stores thier company name, name, price and shipping details in a csv file
+
+Author - Vedant Tilwani
+'''
+
+
 from bs4 import BeautifulSoup as soup 
 from urllib.request import urlopen as uReq 
 
